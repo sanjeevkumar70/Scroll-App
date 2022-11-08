3f08a4aa4c6f642d71e210a3f9a650f43f89f05f
SANJEEV KUMAR <59915362+sk749326@users.noreply.github.com>
2022-11-09 03:49:48 +0530

    update form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,17 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router";
 import "./Login.css";
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const navigate = useNavigate();
-  const handleSubmit = () => {
-    navigate("/home");
-  };
+ 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+      navigate("/home");
+    }
+  
 
   return (
     <div className="form-box">
       <form>
-        <input type="text" placeholder="Username" /> <br /> <br />
-        <input type="text" placeholder="Password" /> <br /> <br />
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <br /> <br />
+        <input
+          type="text"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br /> <br />
         <button onClick={handleSubmit}>Login</button>
       </form>
     </div>
